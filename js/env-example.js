c9831f262d1a12bfffa4dc7a407186c57cf3bcc2
kyok01 <kazutakayokoils@gmail.com>
2022-05-18 16:14:05 +0900

    最初の状態をアップ

---
+++ js/env-example.js
@@ -0,0 +1,14 @@
+// leave your apiKey in
+export const envApiKey = "xxxxxxxxxxxxx";
+// leave your authDomain in
+export const envAuthDomain = "xxxxxxxxxxxxx";
+// leave your databeseURL in
+export const envDatabeseURL = "xxxxxxxxxxxxx";
+// leave your projectId in
+export const envProjectId = "xxxxxxxxxxxxx";
+// leave your storageBucket in
+export const envStorageBucket = "xxxxxxxxxxxxx";
+// leave your messagingSenderId in
+export const envMessagingSenderId = "xxxxxxxxxxxxx";
+// leave your appId in
+export const envApiId = "xxxxxxxxxxxxx";
